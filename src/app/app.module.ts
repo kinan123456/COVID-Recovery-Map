ea244f9526bce62d8267f2957eb793bd1da046e4
kinan123456 <kinansobih29@gmail.com>
2020-04-06 13:18:08 +0300

    Added DataService class
    This will fetch the live data recovered from a web-site and will store it
    in a local variable.
    
    Import tihs service at app-module.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { InformationPanelComponent } from './information-panel/information-panel.component';
+import { DataService } from './shared/data-service.service';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatGridListModule} from '@angular/material/grid-list';
 
@@ -18,7 +19,7 @@ import {MatGridListModule} from '@angular/material/grid-list';
     BrowserAnimationsModule,
     MatGridListModule
   ],
-  providers: [],
+  providers: [DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/shared/data-service.service.ts
@@ -0,0 +1,21 @@
+import { InformationPanelComponent } from '../information-panel/information-panel.component';
+
+export class DataService {
+    info: any;
+    url: string;
+
+    constructor() {
+        this.url = "https://pomber.github.io/covid19/timeseries.json";
+    }
+    // Standard variation
+    extractCovid19Data():  Promise<string> {
+        
+        return fetch(this.url)
+        .then(response => {
+            if (!response.ok) {
+            throw new Error(response.statusText)
+            }
+            return response.json() as Promise<string>
+        })
+    }
+}
