8084ba5be87190b6de82d0c702e5c20837e79c60
kinan123456 <kinansobih29@gmail.com>
2020-04-06 13:17:29 +0300

    Added information-panel component
    This will hold the information on the left side of the application
    (total data recovered of each country)

--- src/app/information-panel/information-panel.component.ts
+++ src/app/information-panel/information-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from '../shared/data-service.service';
 
 @Component({
   selector: 'app-information-panel',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./information-panel.component.css']
 })
 export class InformationPanelComponent implements OnInit {
-
-  constructor() { }
+  info: Promise<string>;
+  totalRecovered: number;
+  constructor(private dataSvc: DataService) { }
 
   ngOnInit(): void {
+    this.totalRecovered = 0;
+    this.dataSvc.extractCovid19Data()
+      .then(data => {
+        let recoveredData = 0;
+        data["Israel"].forEach((date, confirmed, recovered, deaths) => {
+          recoveredData += recovered;
+        }) 
+    });
+
+    this.outputRecovered();
   }
 
+  outputRecovered() {
+    console.log("total is: " + this.totalRecovered)
+  }
 }
