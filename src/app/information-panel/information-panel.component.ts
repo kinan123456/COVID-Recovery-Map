3e8638f65844d1bb4bdd6f01d896c15848ff799d
kinan123456 <kinansobih29@gmail.com>
2020-04-09 13:19:07 +0300

    moved logic from InformationPanelComponent to SearchBarComponent
    this will call the API service to retrieve data and save it in local
    variable which will be iterated and displayed in the Search Bar component.
    
    -added template at HTML file
    
    -removed CSS contnet from SerachBar component

--- src/app/information-panel/information-panel.component.ts
+++ src/app/information-panel/information-panel.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { DataService } from '../shared/data-service.service';
-import { CountryInfo } from './country-info.model';
 
 @Component({
   selector: 'app-information-panel',
@@ -9,30 +7,10 @@ import { CountryInfo } from './country-info.model';
 })
 export class InformationPanelComponent implements OnInit {
   totalRecovered: number;
-  countList: CountryInfo[] = [];
-
-  constructor(private api: DataService) { }
+  
+  constructor() { }
 
   ngOnInit(): void {
-    this.totalRecovered = 0;
-    this.fetchCountriesData();
   }
 
-  fetchCountriesData() {
-    this.api.fetchData()
-      .subscribe(data => {
-        for(const countryName of Object.keys(data)) {
-          let countryConfiremd = 0;
-          let countryRecovered = 0;
-          let countryDeaths = 0;
-
-          data[countryName].forEach(({date, confirmed, recovered, deaths}) => {
-            countryConfiremd += +confirmed
-            countryRecovered += +recovered
-            countryDeaths += +deaths
-          });
-          this.countList.push(new CountryInfo(countryName, countryConfiremd, countryRecovered, countryDeaths));
-        }
-      });
-  }
 }
--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Observable } from 'rxjs';
+import { debounceTime, switchMap, startWith, map, tap, finalize } from 'rxjs/operators';
+import { CountryService } from '../country/country.service';
+import { Country } from '../country/country.model';
+import { MatInput } from '@angular/material/input';
 
 @Component({
   selector: 'app-search-bar',
@@ -6,9 +12,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-bar.component.css']
 })
 export class SearchBarComponent implements OnInit {
-  constructor() { }
+  filteredCountries: Observable<Country[]>;
+  countries: Country[] = [];
+  usersForm: FormGroup;
+  test: any;
 
-  ngOnInit(): void {
-
+  constructor(private fb: FormBuilder, private countrySvc: CountryService) {}
+  
+  ngOnInit() {
+    this.usersForm = this.fb.group({
+      userInput: null
+    });
+    this.filteredCountries = this.usersForm.get('userInput').valueChanges
+    .pipe(
+      debounceTime(300),
+      switchMap(value => this.countrySvc.getCountries())
+    );    
   }
+
+  displayFn(country?: Country): string | undefined {
+    return country ? country.country : undefined;
+  } 
 }
