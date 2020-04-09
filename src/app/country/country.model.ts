74644cf1e2ffc7bb8d6b349e84ebc8e9e068770a
kinan123456 <kinansobih29@gmail.com>
2020-04-09 13:17:41 +0300

    removed CountryInfo and added Country class
    this class contains properties of a country

---
+++ src/app/country/country.model.ts
@@ -0,0 +1,16 @@
+export class Country {
+    country: string;
+    countryInfo: {_id: number, iso2: string, iso3: string, lat: number, long: number, flag: string};
+    updated: number;
+    cases: number;
+    todayCases: number;
+    deaths: number;
+    todayDeaths: number;
+    recovered: number;
+    active: number;
+    critical: number;
+    casesPerOneMillion: number;
+    deathsPerOneMillion: number;
+    tests: number;
+    testsPerOneMillion: number;
+}
--- src/app/information-panel/country-info.model.ts
+++
@@ -1,6 +0,0 @@
-export class CountryInfo {
-    constructor(public name: string, public confirmed: number, 
-                public recovered: number, public deaths: number) {
-                    
-                }
-}
