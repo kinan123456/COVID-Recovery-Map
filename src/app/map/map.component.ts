cb6c82d8d792edc7bceaabf08b8a2a9d4d9423b8
kinan123456 <kinansobih29@gmail.com>
2020-04-11 00:56:52 +0300

    Added HTML and CSS files for Cesium Map
    Added cesium container with start values:
    -updated boolean properties of Cesium.Viewer
    -changed world imagery to contain labels

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -7,6 +7,25 @@
 export class MapComponent implements OnInit {
   constructor() { }
   ngOnInit() {
+  setup() {
+    //////////////////////////////////////////////////////////////////////////
+    // Creating the Viewer & Imagery
+    //////////////////////////////////////////////////////////////////////////
+      imageryProvider: Cesium.createWorldImagery({
+        style: Cesium.IonWorldImageryStyle.AERIAL_WITH_LABELS,
+        assetId: 3954
+      }),
+      baseLayerPicker: false, ////Hide the base layer picker
+      timeline: false,  //Timeline disabled
+      animation: false, //Hide Clock
+      homeButton: false,  //Hide HomeButton top right corner
+      infoBox: false, //InfoBox widget won't be created
+      fullscreenButton: false,
+      sceneModePicker: false,
+      vrButton: true,
+      scene3DOnly: true, //Each geometry instance will only be rendered in 3D to save GPU memory.
+      projectionPicker: true,  //Add projection button
+    });
     });
     // Add map controls
     this.map.addControl(new mapboxgl.NavigationControl());*/
