7217ed73f2d74200936044133a2c897804aa73df
kinan123456 <kinansobih29@gmail.com>
2020-04-12 23:34:18 +0300

    more features for map viewer
    Double click entity to fly to it until limited distance
    added focus for canvas
    refactor code

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -30,11 +30,11 @@ export class MapComponent implements OnInit {
         this.addPins(this.countries[i]);
       }
     });
-
+/*
     //Since some of the pins are created asynchronously, wait for them all to load before zooming/
     Cesium.when(pinMarkers, function(pins){
-      viewer.zoomTo(pins);
-    });
+      viewer.zoomTo(pins)
+    });*/
 
     this.limitZoomAbility();
   }
@@ -59,6 +59,7 @@ export class MapComponent implements OnInit {
           image : canvas.toDataURL(),
           verticalOrigin : Cesium.VerticalOrigin.BOTTOM
         },
+        info: country,
         description: 
                     '\
             <p>\
@@ -109,7 +110,6 @@ export class MapComponent implements OnInit {
       sceneModePicker: false,
       scene3DOnly: true, //Each geometry instance will only be rendered in 3D to save GPU memory.
       projectionPicker: true,  //Add projection button
-      
     });
 
     // Remove credit logo.
@@ -136,10 +136,16 @@ export class MapComponent implements OnInit {
         "Alt: " +
         (cartographic.height * 0.001).toFixed(1) +
         " km";
+
+      viewer.canvas.setAttribute('tabindex', '0'); // needed to put focus on the canvas
+      viewer.canvas.onclick = function() {
+        viewer.canvas.focus();
+      };
     });
 
-    // If the mouse is over a point of interest, change the entity billboard scale and color
-    var previousPickedEntity;
+    //////////////////////////////////////////////////////////////////////////
+    // Custom mouse move interaction for highlighting and selecting entities
+    //////////////////////////////////////////////////////////////////////////
 
     var labelEntity = viewer.entities.add({
       label : {
@@ -150,11 +156,11 @@ export class MapComponent implements OnInit {
           verticalOrigin : Cesium.VerticalOrigin.TOP,
           pixelOffset : new Cesium.Cartesian2(15, 0)
       }
-   });
+    });
 
-    //////////////////////////////////////////////////////////////////////////
-    // Custom mouse interaction for highlighting and selecting
-    //////////////////////////////////////////////////////////////////////////
+    // If the mouse is over a point of interest, change the entity billboard scale and color
+    var previousPickedEntity;
+
     var handler = viewer.screenSpaceEventHandler;
     handler.setInputAction(function(movement) {
       var pickedPrimitive = viewer.scene.pick(movement.endPosition);
@@ -186,5 +192,27 @@ export class MapComponent implements OnInit {
         previousPickedEntity = pickedEntity;
       }
     }, Cesium.ScreenSpaceEventType.MOUSE_MOVE);
+
+    //////////////////////////////////////////////////////////////////////////
+    // Custom mouse double left click interaction for selecting entities
+    //////////////////////////////////////////////////////////////////////////
+    viewer.screenSpaceEventHandler.setInputAction(function(e) {
+      var picked = viewer.scene.pick(e.position);
+      if (Cesium.defined(picked)) {
+          var id = Cesium.defaultValue(picked.id, picked.primitive.id);
+          if (id instanceof Cesium.Entity) {
+              var entity = id;
+              if (Cesium.defined(entity)) {
+                viewer.flyTo(entity, {
+                  offset: new Cesium.HeadingPitchRange(0, -Cesium.Math.PI_OVER_FOUR, 3000000)
+                }).then(function(result) {
+                    if (result) {
+                        viewer.selectedEntity = entity;
+                    }
+                });
+              }
+          }
+      }
+    }, Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);        
   }
 }
