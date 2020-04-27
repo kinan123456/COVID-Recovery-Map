cef6ca6cffb57337801ad633a3f9bb9ea22a1a0c
kinan123456 <kinansobih29@gmail.com>
2020-04-27 19:26:33 +0300

    Identation fix on MapComponent

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit, NgZone, DoCheck, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, NgZone, DoCheck, ChangeDetectorRef, AfterViewInit } from '@angular/core';
 import { CountryService } from '../country/country.service';
 import { Country } from '../country/country.model';
+import { DistanceLegendService } from '../distance-scale/distance-legend.service';
 
 let viewer: any;
 let ellipsoid: any;
 let pinBuilder = new Cesium.PinBuilder();
-
 const MINIMUM_ZOOM_DISTANCE = 250;
 const MAXIMUM_ZOOM_DISTANCE = 12000000;
 const MINIMUM_ZOOM_RATE = 300;
-const DEFAULT_ZOOM_AMOUNT = 2000000;
+const DEFAULT_ZOOM_AMOUNT = 200000;
 
 @Component({
   selector: 'app-map',
@@ -20,7 +20,10 @@ export class MapComponent implements OnInit {
   countries: Country[] = [];
   isLoaded = false;
 
-  constructor(private countrySvc: CountryService, private ngZone: NgZone) { }
+  constructor(private countrySvc: CountryService,
+    private ngZone: NgZone, private distanceSvc: DistanceLegendService) { }
+
+
   ngOnInit() {
     this.ngZone.runOutsideAngular(() => {
       this.setup();
@@ -28,20 +31,51 @@ export class MapComponent implements OnInit {
       this.countrySvc.getCountries().subscribe(resp => {
         this.countries = resp
         for (var i = 0; i < this.countries.length; i++) {
-          this.addPins(this.countries[i]);
+          this.addPins(this.countries[i])
         }
       });
-  
+
       this.limitZoomAbility();
+      let that = this;
+      viewer.camera.changed.addEventListener(function() {
+        that.distanceSvc.updateDistanceLegendCesium(viewer);
+      });
       this.flyToCountry();
     });
   }
 
+      /**
+    * Add pins as markers to all countries at map.
+    * @param country Country object
+    */
+   private addPins(country) {
+    // Create a green marker representing a country.
+    Cesium.when(pinBuilder.fromMakiIconId('marker', Cesium.Color.GREEN, 48), function (canvas) {
+        return viewer.entities.add({
+            id: country.country,
+            position: Cesium.Cartesian3.fromDegrees(country.countryInfo.long, country.countryInfo.lat),
+            billboard: {
+                image: canvas.toDataURL(),
+                verticalOrigin: Cesium.VerticalOrigin.BOTTOM
+            },
+            info: country,
+            description:
+                '<table class="cesium-infoBox-defaultTable"><tbody>' +
+                '<tr><th>Country Name</th><td>' + country.country + '</td></tr>' +
+                '<tr><th>Total Cases</th><td>' + country.cases + '</td></tr>' +
+                '<tr><th>Total Deaths</th><td>' + country.deaths + '</td></tr>' +
+                '<tr><th>Total Recovered</th><td>' + country.recovered + '</td></tr>' +
+                '<tr><th>Total Active</th><td>' + country.active + '</td></tr>' +
+                '</tbody></table>'
+        });
+    });
+}
+
   private limitZoomAbility() {
     viewer.scene.screenSpaceCameraController.minimumZoomDistance = MINIMUM_ZOOM_DISTANCE;
-    viewer.scene.screenSpaceCameraController.maximumZoomDistance = MAXIMUM_ZOOM_DISTANCE;		
+    viewer.scene.screenSpaceCameraController.maximumZoomDistance = MAXIMUM_ZOOM_DISTANCE;
     viewer.scene.screenSpaceCameraController._minimumZoomRate = MINIMUM_ZOOM_RATE;
-  }
+}
 
   private flyToCountry() {
     this.countrySvc.selectedCountry.subscribe(
@@ -50,38 +84,11 @@ export class MapComponent implements OnInit {
         viewer.flyTo(entity, {
           offset: new Cesium.HeadingPitchRange(0, -Cesium.Math.PI_OVER_FOUR, 3000000)
         }).then(function(result) {
-            if (result) {
-              viewer.selectedEntity = entity;
-            }
+          if (result) {
+            viewer.selectedEntity = entity;
+          }
         });
       });
-  }
-  
-  /**
-  * Add pins as markers to all countries at map.
-  * @param country Country object
-  */
- private addPins(country) {
-    // Create a green marker representing a country.
-    Cesium.when(pinBuilder.fromMakiIconId('marker', Cesium.Color.GREEN, 48), function(canvas) {
-      return viewer.entities.add({
-        id: country.country,
-        position : Cesium.Cartesian3.fromDegrees(country.countryInfo.long, country.countryInfo.lat),
-        billboard : {
-          image : canvas.toDataURL(),
-          verticalOrigin : Cesium.VerticalOrigin.BOTTOM
-        },
-        info: country,
-        description: 
-          '<table class="cesium-infoBox-defaultTable"><tbody>' +
-          '<tr><th>Country Name</th><td>' + country.country + '</td></tr>' +
-          '<tr><th>Total Cases</th><td>' + country.cases + '</td></tr>' +
-          '<tr><th>Total Deaths</th><td>' + country.deaths + '</td></tr>' +
-          '<tr><th>Total Recovered</th><td>' +  country.recovered  + '</td></tr>' +
-          '<tr><th>Total Active</th><td>' +  country.active + '</td></tr>' +
-          '</tbody></table>'
-      });
-    });
   }
 
   /**
@@ -106,6 +113,7 @@ export class MapComponent implements OnInit {
     return ellipsoid.cartesianToCartographic(viewer.camera.position).height;
   }
 
+
   private setup() {
     //////////////////////////////////////////////////////////////////////////
     // Creating the Viewer & Imagery
@@ -120,8 +128,8 @@ export class MapComponent implements OnInit {
       animation: false, //Hide Clock
       homeButton: false,  //Hide HomeButton top right corner
       fullscreenButton: false,
-      sceneModePicker: false,
-      scene3DOnly: true, //Each geometry instance will only be rendered in 3D to save GPU memory.
+      sceneModePicker: true,
+      //scene3DOnly: true, //Each geometry instance will only be rendered in 3D to save GPU memory.
       projectionPicker: true,  //Add projection button
       geocoder: false //Disable cesium search
     });
@@ -129,14 +137,14 @@ export class MapComponent implements OnInit {
     var helper = new Cesium.EventHelper();
     helper.add(viewer.scene.globe.tileLoadProgressEvent, function (event) {
       if (event == 0) {
-      setVisible('#loading', false);
+        setVisible('#loading', false);
       }
     });
 
     function setVisible(selector, visible) {
       document.querySelector(selector).style.display = visible ? 'block' : 'none';
     }
-
+    
     // Remove credit logo.
     viewer.scene.frameState.creditDisplay.destroy();
     ellipsoid = viewer.scene.mapProjection.ellipsoid;
@@ -146,13 +154,13 @@ export class MapComponent implements OnInit {
     //////////////////////////////////////////////////////////////////////////
 
     var labelEntity = viewer.entities.add({
-      label : {
-          show : false,
-          showBackground : true,
-          font : '14px monospace',
-          horizontalOrigin : Cesium.HorizontalOrigin.LEFT,
-          verticalOrigin : Cesium.VerticalOrigin.TOP,
-          pixelOffset : new Cesium.Cartesian2(15, 0)
+      label: {
+        show: false,
+        showBackground: true,
+        font: '14px monospace',
+        horizontalOrigin: Cesium.HorizontalOrigin.LEFT,
+        verticalOrigin: Cesium.VerticalOrigin.TOP,
+        pixelOffset: new Cesium.Cartesian2(15, 0)
       }
     });
 
@@ -161,7 +169,7 @@ export class MapComponent implements OnInit {
 
     var hud = document.getElementById("hud");
     var handler = viewer.screenSpaceEventHandler;
-    handler.setInputAction(function(movement) {
+    handler.setInputAction(function (movement) {
 
       displayLongtitudeLatitude(movement);
 
@@ -181,7 +189,7 @@ export class MapComponent implements OnInit {
         Cesium.defined(pickedEntity.billboard)
       ) {
         var cartesian = viewer.scene.pickPosition(movement.endPosition);
-        
+
         labelEntity.position = cartesian;
         labelEntity.label.show = true;
         labelEntity.label.text = pickedEntity.id;
@@ -199,17 +207,18 @@ export class MapComponent implements OnInit {
     function displayLongtitudeLatitude(movement) {
       var cartesian = viewer.camera.pickEllipsoid(movement.endPosition, viewer.scene.globe.ellipsoid);
       if (cartesian) {
-          var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
-          var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2);
-          var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2);
+        var cartographic = Cesium.Cartographic.fromCartesian(cartesian);
+        var longitudeString = Cesium.Math.toDegrees(cartographic.longitude).toFixed(2);
+        var latitudeString = Cesium.Math.toDegrees(cartographic.latitude).toFixed(2);
 
-          hud.innerHTML =
-            'Lon: ' + ('   ' + longitudeString).slice(-7) + '\u00B0' +
-            '\nLat: ' + ('   ' + latitudeString).slice(-7) + '\u00B0';
+        hud.innerHTML =
+          'Lon: ' + ('   ' + longitudeString).slice(-7) + '\u00B0' +
+          '\nLat: ' + ('   ' + latitudeString).slice(-7) + '\u00B0';
       } else {
         hud.innerHTML = '';
-      } 
+      }
     }
+
     //////////////////////////////////////////////////////////////////////////
     // Custom mouse double left click interaction for selecting entities
     //////////////////////////////////////////////////////////////////////////
@@ -219,18 +228,18 @@ export class MapComponent implements OnInit {
       viewer.scene.canvas.focus();
       var picked = viewer.scene.pick(e.position);
       if (Cesium.defined(picked)) {
-          var id = Cesium.defaultValue(picked.id, picked.primitive.id);
-          if (id instanceof Cesium.Entity) {
-              var entity = id;
-              if (Cesium.defined(entity)) {
-                viewer.flyTo(entity, {
-                  offset: new Cesium.HeadingPitchRange(0, -Cesium.Math.PI_OVER_FOUR, 3000000)
-                });
-                viewer.selectedEntity = entity;
-                this.ngZone.run(() => this.countrySvc.updateCountry(entity.info as Country));
-              }
+        var id = Cesium.defaultValue(picked.id, picked.primitive.id);
+        if (id instanceof Cesium.Entity) {
+          var entity = id;
+          if (Cesium.defined(entity)) {
+            viewer.flyTo(entity, {
+              offset: new Cesium.HeadingPitchRange(0, -Cesium.Math.PI_OVER_FOUR, 3000000)
+            });
+            viewer.selectedEntity = entity;
+            this.ngZone.run(() => this.countrySvc.updateCountry(entity.info as Country));
           }
+        }
       }
     }, Cesium.ScreenSpaceEventType.LEFT_DOWN);
-  }
+  }  
 }
